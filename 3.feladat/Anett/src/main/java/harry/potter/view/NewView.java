9572563f545fb8fb49031119b35123fcab2b0686
akosztik <akosztik@gmail.com>
2017-11-27 19:07:40 +0100

    push

--- 3.feladat/Anett/src/main/java/harry/potter/view/NewView.java
+++ 3.feladat/Anett/src/main/java/harry/potter/view/NewView.java
@@ -31,7 +31,7 @@ public class NewView{
             @Override
             public void actionPerformed(ActionEvent e) {
                 System.out.println("new Item: " +text.getText() + " , " );
-                
+
             }
         });
 
